57c3996f336acf3d7c6ebd1cf423b4c5fe44691e
Kendal Percimoney <48430773+kendalpercimoney@users.noreply.github.com>
2023-03-05 21:23:23 +0000

    Updated so that the verses and images update daily

--- edifyAPI.php
+++ edifyAPI.php
@@ -1,9 +1,94 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    $data = array(
-        'imageUrl' => 'https://firstiimpression.com/Edify/images/3_tobias-wilden-ZWeLtP4kQlA-unsplash.jpg',
-        'verse' => 'Proverbs 3:5 - Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.'
-    );
-    header('Content-Type: application/json');
-    echo json_encode($data);
-    
+header("Access-Control-Allow-Origin: *");
+
+// Set the timezone to your local timezone
+date_default_timezone_set('Europe/London');
+
+// Generate the URL for the image
+$imageNumber = (date('z') + 1); // Use the day of the month as the image number
+$imageUrl = 'https://firstiimpression.com/Edify/images/' . $imageNumber . '.jpg';
+
+// Generate the verse for the day
+$verses = array(
+    'Day 1',
+    'Day 2',
+    'Day 3',
+    'Day 4',
+    'Day 5',
+    'Day 6',
+    'Day 7',
+    'Day 8',
+    'Day 9',
+    'Day 10',
+    'Day 11',
+    'Day 12',
+    'Day 13',
+    'Day 14',
+    'Day 15',
+    'Day 16',
+    'Day 17',
+    'Day 18',
+    'Day 19',
+    'Day 20',
+    'Day 21',
+    'Day 22',
+    'Day 23',
+    'Day 24',
+    'Day 25',
+    'Day 26',
+    'Day 27',
+    'Day 28',
+    'Day 29',
+    'Day 30',
+    'Day 31',
+    'Day 32',
+    'Day 33',
+    'Day 34',
+    'Day 35',
+    'Day 36',
+    'Day 37',
+    'Day 38',
+    'Day 39',
+    'Day 40',
+    'Day 41',
+    'Day 42',
+    'Day 43',
+    'Day 44',
+    'Day 45',
+    'Day 46',
+    'Day 47',
+    'Day 48',
+    'Day 49',
+    'Day 50',
+    'Day 51',
+    'Day 52',
+    'Day 53',
+    'Day 54',
+    'Day 55',
+    'Day 56',
+    'Day 57',
+    'Day 58',
+    'Day 59',
+    'Day 60',
+    'Day 61',
+    'Day 62',
+    'Day 63',
+    'Psalm 23:4 - I will fear no evil; For You are with me; Your rod and Your staff, they comfort me.',
+    'Psalm 27:1 - The Lord is my light and my salvation; Whom shall I fear? The Lord is the strength of my life; Of whom shall I be afraid?',
+    'Isaiah 41:10 - Fear not, for I am with you; Be not dismayed, for I am your God. I will strengthen you, Yes, I will help you, I will uphold you with My righteous right hand.',
+    // Add more verses here
+);
+$verse = $verses[date('z') % count($verses)]; // Use the day of the year to select a verse
+
+// Create an array with the new data
+$data = array(
+    'imageUrl' => $imageUrl,
+    'verse' => $verse
+);
+
+// Save the new data to a JSON file
+file_put_contents('data.json', json_encode($data));
+
+// Return the new data as a JSON response
+header('Content-Type: application/json');
+echo json_encode($data);
